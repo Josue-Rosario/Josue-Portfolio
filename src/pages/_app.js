97eeadda1f9c8704fbaabc92baedb7f58d8ec5d9
Josue Rosario <46581635+Josuerosario21@users.noreply.github.com>
2022-04-19 11:04:28 -0500

    Delete yarn.lock

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,7 @@
 import Theme from "../styles/theme";
 import Script from "next/script";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as ga from "../lib/google-analytics";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
       <Script
